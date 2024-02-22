d3089de72c49460dd402661b792c5d1ad3eef25f
András Guseo <andras@theeventscalendar.com>
2024-02-22 16:47:15 +0100

    Add comments to scripts

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,21 +1,32 @@
 const timeout = setTimeout(updateNotices, 200);
 
+const selectors = [
+    ".wrap > .notice:not(.updated):not(.hidden)",   // Generic
+    "#wpbody-content > .notice:not(.updated)"       // WordPress update
+];
+
 function updateNotices() {
-    const selectors = [
-        ".wrap > .notice:not(.updated):not(.hidden)",   // Generic
-        "#wpbody-content > .notice:not(.updated)"       // WordPress update
-    ];
     let num = document.querySelectorAll(selectors.toString());
+
+    // Need to remove our own.
+    let numberOfNotices = num.length-1;
+
+    // Update the number of notices in the bubbles.
     if (num.length > 0) {
         document.getElementById('notification-count').innerHTML = num.length;
         document.getElementById('admin-bar-notification-count').innerHTML = num.length;
 
+        // Hide "hurray" if there are notices.
         const onpage = document.querySelector('.dashboard-notifications #content');
         if ( onpage != null ) {
             onpage.remove();
         }
     } else {
+        // Hide bubble in menu.
         document.getElementById('notification-count').style.display = 'none';
+        // Hide bubble in toolbar.
         document.getElementById('admin-bar-notification-count').style.display = 'none';
+        // Hide "Notifications" notice.
+        document.getElementById('notice--dashboard-notifications').style.display = 'none';
     }
 }
